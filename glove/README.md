738fc894004b105f60f712e98ac45b2c73a2d656
Wave <rover9632@gmail.com>
2018-06-24 23:11:57 +0800

    Add glove tool

---
+++ glove/README.md
@@ -0,0 +1,5 @@
+This is a tool that used to train word vectors using GloVe model.
+
+It's from : https://github.com/stanfordnlp/glove.
+
+I just made a little changes on it for this project.
---
+++ glove/word_gen.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# trains a GloVe model
+
+CORPUS=../data/corpus_word.txt
+VOCAB_FILE=../data/vocab_word.txt
+COOCCURRENCE_FILE=cooccurrence.bin
+COOCCURRENCE_SHUF_FILE=cooccurrence.shuf.bin
+BUILDDIR=build
+SAVE_FILE=../data/vectors_word
+VERBOSE=2
+MEMORY=4.0
+VOCAB_MIN_COUNT=2
+VECTOR_SIZE=300
+MAX_ITER=20
+WINDOW_SIZE=8
+BINARY=2
+NUM_THREADS=8
+X_MAX=100
+
+echo "$ $BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE"
+$BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE
+echo "$ $BUILDDIR/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE"
+$BUILDDIR/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE
+echo "$ $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE"
+$BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE
+echo "$ $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE"
+$BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
