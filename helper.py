64ac18511a407fac934a69a6b317629ae5f5c3d4
Wave <rover9632@gmail.com>
2018-06-24 22:08:26 +0800

    Modify file path references and add a function of generating corpus with word level

--- helper.py
+++ helper.py
@@ -6,38 +6,46 @@ import re
 import random
 import sys
 
-def train_test_split(fnames, test_size=0.1):
+def train_test_split(in_data_paths, test_size=0.1):
     x = []
-    for fname in fnames:
-        with open(fname) as f:
+    for fpath in in_data_paths:
+        with open(fpath) as f:
             for line in f:
                 x.append(line.split('\t')[1:])
 
     random.shuffle(x)
     p = int(len(x) * test_size)
+    out_data_paths = ['./data/train.csv', './data/validation.csv']
 
-    for data,fname in zip([x[:-p], x[-p:]], ['./train.csv', './validation.csv']):
-        with open(fname, 'w') as f:
+    for data,fpath in zip([x[:-p], x[-p:]], out_data_paths):
+        with open(fpath, 'w') as f:
             i = 1
             for item in data:
                 f.write('\t'.join([str(i)]+item))
                 i += 1
     pass
 
-def data2corpus(data, corpus):
-    with open('./punctuation.txt') as f:
-        punctuation = f.read().decode('utf-8').split()
+def data2corpus(data_path, corpus_path, wordlevel=False):
+    with open('./data/punctuations.txt') as f:
+        punctuations = f.read().decode('utf-8').split()
 
-    with open(data) as fi, open(corpus, 'w') as fo:
+    if wordlevel:
+        jb.load_userdict('./data/dict.txt')
+
+    with open(data_path) as fi, open(corpus_path, 'w') as fo:
         for line in fi:
             line = re.sub(u'[。？！.?!]', u'\n', line.decode('utf-8'))
             _, sen1, sen2, _ = line.split(u'\t')
-            sen1 = [w for w in sen1 if w not in punctuation]
-            sen2 = [w for w in sen2 if w not in punctuation]
+            if wordlevel:
+                sen1, sen2 = jb.lcut(sen1), jb.lcut(sen2)
+            sen1 = [w for w in sen1 if w not in punctuations]
+            sen2 = [w for w in sen2 if w not in punctuations]
             fo.write((u' '.join(sen1) + u'\n').encode('utf-8'))
             fo.write((u' '.join(sen2) + u'\n').encode('utf-8'))
     pass
 
+
 if __name__ == '__main__':
     #train_test_split(sys.argv[1:])
-    data2corpus('./train.csv', './corpus.txt')
+    data2corpus('./data/train.csv', './data/corpus_char.txt')
+    data2corpus('./data/train.csv', './data/corpus_word.txt', True)
